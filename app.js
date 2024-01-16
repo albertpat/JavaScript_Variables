91c04db0f061311ebb59ab3c9f5a34c4220cf4fc
Albert Pattarachote <albertpattarachote22@gmail.com>
2024-01-16 07:07:45 -1000

    JavaScript vars

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName = "Pattarachote";
+var age = "23";
 // Exercise 2
+const firstName = "Albert";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
